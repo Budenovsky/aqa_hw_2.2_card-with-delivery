3b157968f3044f2bf9cea8f50b27049caf21646c
Boris.Budenovsky <fpqb00@gmail.com>
2021-03-10 16:20:58 +0300

    added badge

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/mww9sgsmlfy0l27m?svg=true)](https://ci.appveyor.com/project/Budenovsky/aqa-hw-2-2-card-with-delivery)
--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -1,2 +1,38 @@
-package ru.netology.web;public class CardDeliveryTest {
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.ExactText;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+    @Test
+    void shouldSendForm() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Москва");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DAY_OF_MONTH, 3);
+        String minDeliveryDate = sdf.format(cal.getTime());
+        $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
+        $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(minDeliveryDate); //todo add logic with date
+        $("[data-test-id='name'] input").setValue("Вероника Степанова");
+        $("[data-test-id='phone'] input").setValue("+79990001122");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        Configuration.timeout = 15000;
+        $(withText("Успешно!"));
+    }
 }
