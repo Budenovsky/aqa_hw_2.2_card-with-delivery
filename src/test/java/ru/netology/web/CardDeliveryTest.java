3be72c69d526feea45b6e194a293f9662ae1ea8d
Boris.Budenovsky <fpqb00@gmail.com>
2021-03-12 13:04:40 +0300

    changed date logic

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -7,32 +7,37 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
 import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    String getDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     void shouldSendForm() {
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Москва");
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 3);
-        String minDeliveryDate = sdf.format(cal.getTime());
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "A");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(minDeliveryDate); //todo add logic with date
+        String minDeliveryDate = getDate(3);
+        $("[data-test-id='date'] input").setValue(minDeliveryDate);
         $("[data-test-id='name'] input").setValue("Вероника Степанова");
         $("[data-test-id='phone'] input").setValue("+79990001122");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        Configuration.timeout = 15000;
-        $(withText("Успешно!"));
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно забронирована на " + minDeliveryDate));
     }
 }
